50c8da5af4e2c8888141eb083d49abd5025f4a20
marcelo <mhosan@gmail.com>
2022-01-29 12:54:13 -0300

    no funca la libreria de status codes

--- network/response.js
+++ network/response.js
@@ -1,3 +1,6 @@
+const httpStatus = require("http-status-codes");
+
+/* 
 exports.success = (req, res, message, status) => {
     let statuCode = status || 200;
     let statusMessage = message || 'OK';
@@ -8,7 +11,16 @@ exports.success = (req, res, message, status) => {
         body: statusMessage
     });
 }
+ */
+exports.success = (req, res, message = "", status = httpStatus.OK) => {
+    res.status(statu).send({
+        error: false,
+        status: status,
+        body: message
+    });
+}
 
+/* 
 exports.error = (req, res, message, status) => {
     let statuCode = status || 500;
     let statusMessage = message || 'Internal server error';
@@ -17,5 +29,12 @@ exports.error = (req, res, message, status) => {
         error: false,
         status: statuCode,
         body: statusMessage
+    });
+} */
+exports.error = (req, res, message = "Internal server error", status = httpStatus.INTERNAL_SERVER_ERROR) => {
+    res.status(status).send({
+        error: true,
+        status: status,
+        body: message
     });
 }
