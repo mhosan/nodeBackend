523047eee6e37c3dd59231a5c062369cc7c991ec
marcelo <mhosan@gmail.com>
2022-01-31 09:49:02 -0300

    ahora con la bd de mentira

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const response = require('../../../network/response');
+const Controller = require('./controller');
 const router = express.Router();
+
 router.get('/', (req, res) => {
     //res.send('Todo funca ok');
-    response.success(req, res, 'Todo funca ok', 200);
+    const lista = Controller.list();
+    response.success(req, res, lista, 200);
 });
 
 module.exports = router;
